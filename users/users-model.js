90ae4723e2844ed328ec4a2b4ae56b0282e42e39
Bphilipson2 <bradley.s.philipson@gmail.com>
2019-12-20 22:19:03 -0500

    new files

---
+++ users/users-model.js
@@ -0,0 +1,30 @@
+const db = require('PLACEHOLDER');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('users').select('id', 'username');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db('users').select('username')
+    .where({ id })
+    .first();
+}
