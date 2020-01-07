56839277036476ecab2126cbc8ff6246018eb55d
Bphilipson2 <bradley.s.philipson@gmail.com>
2020-01-06 21:35:30 -0500

    quick on the route

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const secrets = require('./secrets.js');
+const secrets = require('../config/secrets');
 
 const environment = secrets.environment || 'development';
 const config = require('../knexfile.js')[environment];
