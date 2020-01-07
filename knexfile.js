cc14815a4ca4e2300cd9338a1e06820b3db408da
Bphilipson2 <bradley.s.philipson@gmail.com>
2020-01-06 22:50:29 -0500

    file changes

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,47 @@
-//Heroku will provide the database_url
-
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './database/salt.db3',
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done);
-        },
-      },
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/salt.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
-    testing: {
-      client: 'sqlite3',
-      connection: {
-        filename: './database/test.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
+    migrations: {
+      directory: './database/migrations',
     },
-    production: {
-      client: 'pg',
-      connection: process.env.DATABASE_URL, // heroku sets this
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
+    seeds: {
+      directory: './database/seeds',
     },
-    datascience: {
-      client: 'pg', 
-      connection: process.env.DS_DATABASE_URL, // heroku sets this
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
     },
-  };
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+  datascience: {
+    client: 'pg',
+    connection: process.env.DS_DATABASE_URL,
+  },
+};
